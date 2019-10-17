d760ac3d76f11d0e47797927e7e87e501f5c8bfd
IndrekV <indzuliin@gmail.com>
2019-10-17 16:21:26 +0300

    Added circular tracing progressmeter

--- app/ui.js
+++ app/ui.js
@@ -3,8 +3,14 @@ import clock from "clock";
 
 clock.granularity = "seconds";
 
-var runningEntry = null
+var runningEntry = null;
 var durationLabel = null;
+var lastTo = 0;
+
+const secondsContainer = document.getElementById("arc-seconds");
+const secondsContainerBack = document.getElementById("arc-seconds-back");
+const secondsAnim = secondsContainer.getElementById("anim");
+const secondsArc = secondsContainer.getElementById("arc");
 
 export function UI() {
   this.status = document.getElementById("status");
@@ -29,15 +35,28 @@ UI.prototype.updateTimer = function(data) {
 
   runningEntry = data;
   this.entry = data;
-  this.status.text = (!!data) ? "Stop": "Start";
+  this.status.text = (!!data) ? "◻": ">";
   if (!!data) {
+    //Running entry
     console.log("Description - " + data.description);
-    this.entryLabel.text = data.description;
-    this.circle.style.fill = "red";
+    this.entryLabel.text = (!!data.description)? data.description: "(no description)";
+    this.circle.style.fill = "#db1e1e";
+    toggleArc(true);
   } else {
     durationLabel.text = "";
     this.entryLabel.text = "";
     this.circle.style.fill = "#228B22";
+    toggleArc(false);
+  }
+}
+
+var toggleArc = function(show) {
+  if (show) {
+    secondsContainer.style.display = "inline";
+    secondsContainerBack.style.display = "inline";
+  } else {
+    secondsContainer.style.display = "none";
+    secondsContainerBack.style.display = "none";
   }
 }
 
@@ -48,6 +67,14 @@ var updateDuration = function() {
   let minutes = parseInt((duration / (1000 * 60)) % 60, 10);
   let hours = parseInt(duration / (1000 * 60 * 60), 10);
 
+  // circle animation
+  secondsAnim.to = calcArc(seconds, 60);
+  secondsAnim.from = lastTo;
+
+  lastTo = secondsAnim.to;
+  //console.log(secondsAnim.from + " -> " + secondsAnim.to + " = " + seconds);
+  secondsContainer.animate("enable");
+
   hours = hours < 10 ? '0' + hours : hours;
   minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
@@ -55,9 +82,14 @@ var updateDuration = function() {
   durationLabel.text = hours + ':' + minutes + ':' + seconds;
 }
 
+function calcArc(current, steps) {
+  let angle = (360 / steps) * current;
+  console.log ("Angle: " + angle);
+  return angle > 360 ? 360 : angle;
+}
+
 clock.ontick = e => {
   if (!!runningEntry) {
     updateDuration();
   }
-  
 }
